9b3968b97652114d11cbddbe04bbb180ca061e69
unknown <ppkpppk@gmail.com>
2014-03-31 19:21:32 +0800

    initial

---
+++ slide_config.js
@@ -0,0 +1,25 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Callback, EventEmitter and Promise',
+    subtitle: 'JavaScript中的异步调用',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    favIcon: 'favicon.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: '岩桐',
+    company: '信息中心<br>心物不二',
+    mobile: '13426121613',
+    qq: '87038567',
+    github: 'http://github.com/ppkpppk'
+  }]
+};
+
